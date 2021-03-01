edd3decaf3b0e782126749e8847c1d525bca6cb6
Abdul Wassay Ansari <abdulwassayansari786@gmail.com>
2021-03-01 17:52:54 +0500

    Quiz App

---
+++ app.js
@@ -0,0 +1,12 @@
+function check(){
+        var c = 0 ;
+        var q1 = document.quiz.question1.value;
+        var q2 = document.quiz.question2.value;
+        if (q2 == "option1"){
+            c++
+        }
+        if (q2 == "option1"){
+            c++
+        }
+        alert("Your score is " + c)
+    }
